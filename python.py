f90c3a52130c363d4a1b5049c4ae1e871b72d364
AnkitV15 <av5120634@gmail.com>
2024-01-21 12:19:39 +0530

    the first version of app is here

--- python.py
+++ python.py
@@ -1,3 +1,4 @@
+this is the first version of app
 this is a change from feature branch
 
 this is a change from feature branch
--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,5 @@
+this is the first version of git
+
 This is a change from feature branch This is a change from feature branch This 
 is a change from feature branch This is a change from feature branch This is a 
 change from feature branch This is a change from feature branch This is a 
